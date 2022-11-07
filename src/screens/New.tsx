0bd1f3548b5cce8628947cb8365a0a03136d66bf
aly <alyssonrosa1@gmail.com>
2022-11-06 21:27:35 -0300

    create new pool

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,10 +1,51 @@
-import { Heading, Text, VStack } from "native-base";
+import { Heading, Text, Toast, useToast, VStack } from "native-base";
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import Logo from "../assets/logo.svg"
+import { useState } from "react";
+import { api } from "../services/api";
 
 export function New() {
+    const [title, setTitle] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toast = useToast();
+
+    const handlePoolCreate = async () => {
+        if (!title) {
+            return toast.show({
+                title: 'Informe um nome para seu bolão',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
+
+        try {
+            setIsLoading(true)
+
+            await api.post('pools', { title })
+
+            toast.show({
+                title: 'Bolão criado com sucesso',
+                placement: 'top',
+                bgColor: 'green.500'
+            })
+
+            setTitle('')
+        } catch (error) {
+            console.log(error)
+
+            toast.show({
+                title: 'Não foi possível criar o bolão',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <VStack flex={1} bgColor="gray.800">
             <Header title="Criar Novo Bolão" />
@@ -17,8 +58,18 @@ export function New() {
                     e compartilhe entre amigos!
                 </Heading>
 
-                <Input mb={2} placeholder="Qual nome do seu bolão?" />
-                <Button title="Criar meu Bolão" />
+                <Input
+                    mb={2}
+                    placeholder="Qual nome do seu bolão?"
+                    onChangeText={(value) => setTitle(value.trim())}
+                    value={title}
+                />
+
+                <Button
+                    title="Criar meu Bolão"
+                    onPress={handlePoolCreate}
+                    isLoading={isLoading}
+                />
 
                 <Text color="gray.200" textAlign="center" mt={4}>
                     Após criar seu bolão, você receberá um {'\n'}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,9 +3,10 @@ import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
 import { Fontisto } from '@expo/vector-icons'
 import { useAuth } from "../hooks/useAuth";
+import { color } from "native-base/lib/typescript/theme/styled-system";
 
 export function SignIn() {
-    const { signIn, user } = useAuth();
+    const { signIn, isUserLoading } = useAuth();
 
     return (
         <Center flex={1} bgColor="gray.800" p={7}>
@@ -17,6 +18,8 @@ export function SignIn() {
                 leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
                 mt="12"
                 onPress={signIn}
+                isLoading={isUserLoading}
+                _loading={{ _spinner: { color: 'white' } }}
             />
 
             <Text color="gray.200" textAlign="center" mt="4">
