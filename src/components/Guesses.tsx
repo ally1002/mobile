3a7b9583853ae0b40b08d0cb2d226acfffe4a0b9
aly <alyssonrosa1@gmail.com>
2022-11-10 20:03:00 -0300

    mobile

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -2,13 +2,15 @@ import { useToast, FlatList } from 'native-base';
 import { useState, useEffect } from 'react';
 
 import { api } from '../services/api';
+import { EmptyMyPoolList } from './EmptyMyPoolList';
 import { Game, GameProps } from './Game';
 import { Loading } from './Loading';
 interface Props {
   poolId: string;
+  code: string;
 }
 
-export function Guesses({ poolId }: Props) {
+export function Guesses({ poolId, code }: Props) {
   const [isLoading, setIsLoading] = useState(true);
   const [games, setGames] = useState<GameProps[]>([]);
   const [firstTeamPoints, setFirstTeamPoints] = useState('');
@@ -91,6 +93,7 @@ export function Guesses({ poolId }: Props) {
         />
       )}
       _contentContainerStyle={{ pb: 10 }}
+      ListEmptyComponent={() => <EmptyMyPoolList code={code} />}
     />
   );
 }
